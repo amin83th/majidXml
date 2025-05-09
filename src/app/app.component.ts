241b7d910165e8e572f7ef8765bc27b373ec232e
amin83th <mohammadamin83th@gmail.com>
2025-05-09 16:32:24 +0330

    from recursive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GetInputComponent } from "../components/get-input/get-input.component";
+import { TemplateComponent } from "../components/template/template.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GetInputComponent],
+  imports: [RouterOutlet, GetInputComponent, TemplateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'majidXml';
+  file: any;
+
+  getFile(event: any) {
+    this.file = event;
+  }
 }
--- src/components/template/template.component.ts
+++ src/components/template/template.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template.component.html',
   styleUrl: './template.component.css'
 })
 export class TemplateComponent {
+
+  @Input() fileInput: any;
+
+
+  finallyFile: any;
+
+  ngOnChanges() {
+    console.log('فایل دریافت شد:', this.fileInput);
+
+    this.finallyFile = this.fileInput;
+  }
 
 }
